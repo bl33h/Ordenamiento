285e3ee5aaf5b922efdd90a36683b7960b95254f
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-02-23 18:41:33 -0600

    Creacion del comparador

---
+++ src/IComparator.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C), 2022-2023, FabianJuarez SaraEcheverria Jose Pablo Kiesling
+ * FileName: Controladora
+ * Author:   Fabián Juárez , Sara Echeverria , Jose Pablo Kiesling
+ * Date:     4/03/2022
+ * @author Jose Pablo Kiesling, Fabian Juarez y Sara Echeverria
+ */
+public interface IComparator<T> {
+	public int Compare(T _object1, T _object2);
+}
